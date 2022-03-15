cb81e0b51966d62de34d4d97359c6a22fff9ef82
nxxh447 <65509800+nxxh447@users.noreply.github.com>
2022-03-15 16:35:23 -0500

    ..

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const sendMessage = (whURl)	=> {
+function sendMessage(whURl) {
 	new XMLhttpRequest().open("POST", whURL)
 		.setRequestHeader("Content-type", "application/json")
-		.send(JSON.stringify(...data));
+		.send(JSON.stringify(data));
 }
